76d90f8db973d9179c68eb1ba01f90bd523494aa
Leon Bryan <leonard.bryan.email@gmail.com>
2025-09-29 02:26:24 +0800

    Content check.

--- src/components/content/GuidesContent.jsx
+++ src/components/content/GuidesContent.jsx
@@ -6,7 +6,7 @@ const GuidesContent = [
         title: "How to activate Windows and MS Office",
         content: `
 <br><h2><strong>The Manual Method</strong></h2>
-<p><strong>&ensp;&ensp;1.</strong> Go to massgravel's <a href="https://github.com/massgravel/Microsoft-Activation-Scripts"><strong>Microsoft-Activation-Scrips</strong></a> github repository.</p>
+<p><strong>&ensp;&ensp;1.</strong> Go to massgravel's <a href="https://github.com/massgravel/Microsoft-Activation-Scripts" target="_blank"><strong>Microsoft-Activation-Scrips</strong></a> github repository.</p>
 <br><p><strong>&ensp;&ensp;2.</strong> Click on the <strong>Code</strong> button and click <strong>Download Zip</strong></p>
 <p><strong>&ensp;&ensp;3.</strong> Once downloaded, extract the files anywhere.</p>
 <p><strong>&ensp;&ensp;4.</strong> Navigate to <code>Microsoft-Activation-Scripts-master/MAS/All-In-One-Version-KL</code>.</p>
@@ -15,16 +15,17 @@ const GuidesContent = [
 <h2><strong>The Powershell Method</strong></h2>
 <p><strong>&ensp;&ensp;1.</strong> Open PowerShell (Not CMD).</p>
 <p><strong>&ensp;&ensp;2.&nbsp;</strong>Copy and paste the command:</p>
-<pre class="notranslate">&ensp;&ensp;&ensp;&ensp;<code>irm https://get.activated.win | iex</code></pre>`
+<pre class="notranslate">&ensp;&ensp;&ensp;&ensp;<code>irm https://get.activated.win | iex</code></pre>
+`
     },
     {
         id: "my_modal_2",
         title: "How to patch Spotify Desktop (Windows)",
         content: `
 <br><h3><strong>Microsoft Store version is not supported</strong></h3>
-<p>Download it from directly from <a href="https://download.scdn.co/SpotifySetup.exe"><strong>here</strong></a></p>
+<p>Download it from directly from <a href="https://download.scdn.co/SpotifySetup.exe" target="_blank"><strong>here</strong></a></p>
 <h2><strong>Patching</strong></h2>
-<p><strong>&ensp;&ensp;1.</strong> Go to amd64fox's <strong><a href="https://github.com/SpotX-Official/SpotX">SpotX</a></strong> github repository.</p>
+<p><strong>&ensp;&ensp;1.</strong> Go to amd64fox's <strong><a href="https://github.com/SpotX-Official/SpotX" target="_blank">SpotX</a></strong> github repository.</p>
 <p><strong>&ensp;&ensp;2.</strong> Click the <strong>Code</strong> button, then click <strong>Download Zip</strong></p>
 <p><strong>&ensp;&ensp;3.</strong> Once downloaded, extract the files anywhere.</p>
 <p><strong>&ensp;&ensp;4.</strong> Navigate to the <code>SpotX-main</code> folder.</p>
